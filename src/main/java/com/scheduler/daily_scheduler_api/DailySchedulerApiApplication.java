14f246989774ee872b6a3ef1ef2cc7744e274ec4
fkakd1205 <fkakd0102@naver.com>
2022-02-26 21:48:30 +0900

    first commit

---
+++ src/main/java/com/scheduler/daily_scheduler_api/DailySchedulerApiApplication.java
@@ -0,0 +1,13 @@
+package com.scheduler.daily_scheduler_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailySchedulerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailySchedulerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scheduler/daily_scheduler_api/DailySchedulerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduler.daily_scheduler_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailySchedulerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
